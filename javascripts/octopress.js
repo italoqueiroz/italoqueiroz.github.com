bffd63d5738bed2422830e73874b57320c79d467
Italo Queiroz <italoqueiroz@gmail.com>
2012-10-23 15:33:24 -0200

    Site updated at 2012-10-23 17:33:24 UTC

--- javascripts/octopress.js
+++ javascripts/octopress.js
@@ -1,12 +1,16 @@
 function getNav() {
   var mobileNav = $('nav[role=navigation] fieldset[role=search]').after('<fieldset class="mobile-nav"></fieldset>').next().append('<select></select>');
-  mobileNav.children('select').append('<option value="">Navigate&hellip;</option>');
+  mobileNav.children('select').append('<option value="">Menu&hellip;</option>');
   $('ul[role=main-navigation]').addClass('main-navigation');
   $('ul.main-navigation a').each(function(link) {
     mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
   });
   $('ul.subscription a').each(function(link) {
-    mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
+		if ($(link).find('img')) {
+			mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+$(link).find('img').attr('alt')+'</option>');
+		} else {
+			mobileNav.children('select').append('<option value="'+link.href+'">&raquo; '+link.text+'</option>');
+		}
   });
   mobileNav.children('select').bind('change', function(event) {
     if (event.target.value) { window.location.href = event.target.value; }
