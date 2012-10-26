85d832fcee3d013d08a96de4894dc9e5e684c084
Italo Queiroz <italoqueiroz@gmail.com>
2012-10-26 00:47:33 -0200

    Site updated at 2012-10-26 02:47:33 UTC

---
+++ javascripts/custom.js
@@ -0,0 +1,25 @@
+$(function(){
+  $('[rel="tooltip"]').tooltip();
+
+  if ( window.location.pathname == '/' ) {
+    $('[rel="tooltip"]').hover(
+      function () {
+        $('.tooltip.top').addClass("home");
+      }
+    );
+  }
+
+  $(".tweet").tweet({
+    username: 'italoqueirozdf',
+    join_text: "auto",
+    avatar_size: 32,
+    count: 3,
+    loading_text: "carregando tweets...",
+    auto_join_text_default: "",
+    auto_join_text_ed: "",
+    auto_join_text_ing: "",
+    auto_join_text_reply: "",
+    auto_join_text_url: "",
+    loading_text: "carregando tweets..."
+  });
+});
